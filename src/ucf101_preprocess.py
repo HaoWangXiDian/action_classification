437b8056324ce96f3ea84b0484d03e877f8e14f1
behrooz <behrooz.mahasseni@gmail.com>
2016-12-22 15:42:02 -0800

    initial commit

---
+++ src/ucf101_preprocess.py
@@ -0,0 +1,60 @@
+'''
+Created on Dec 22, 2016
+
+@author: Behrooz
+'''
+# this lines add the sequence classification submodule to the python path
+import sys
+sys.path.append('../sequence_analysis/src')
+
+#############################
+
+#############################
+import os
+import tensorflow as tf
+import numpy as np
+
+train_files = ['../ucf_train_test_files/trainlist01.txt', '../ucf_train_test_files/trainlist02.txt', '../ucf_train_test_files/trainlist03.txt']
+test_files = ['../ucf_train_test_files/testlist01.txt', '../ucf_train_test_files/testlist02.txt', '../ucf_train_test_files/testlist03.txt']
+
+def modify_test_files():
+    class_map = {}
+    class_ids = open('../ucf_train_test_files/classInd.txt')
+    for line in class_ids:
+        s = line.split(' ')
+        class_map[s[1].strip()] = int(s[0])
+    for file_name in test_files:
+        file_content = []
+        file = open(file_name, 'r')
+        for line in file:
+            label = class_map[line.split('/')[0]]
+            file_content.append(line.strip() + ' ' + str(label) + '\n')
+        file = open( file_name + 'm', 'w')
+        file.writelines(file_content)
+    
+def generate_train():
+    for file_name in train_files:
+        file = open(file_name, 'r')
+        for line in file:
+            s = line.split(' ')
+            label = int(s[1].strip())
+            feature_file = s[0].split('/')[1][:-4] + '.npy'
+            features = np.load(feature_file)
+            print(label, features.shape)
+
+
+def generate_test():
+    for file_name in train_files:
+        file = open(file_name, 'r')
+        for line in file:
+            s = line.split(' ')
+            label = int(s[1].strip())
+            feature_file = s[0].split('/')[1][:-4] + '.npy'
+            features = np.load(feature_file)
+            print(label, features.shape)
+def main():
+#     modify_test_files()
+    generate_train()
+if __name__ == "__main__":
+#   flags.FLAGS(sys.argv)
+    main()
---
+++ src/ucf101_train.py
@@ -0,0 +1,85 @@
+'''
+Created on Dec 22, 2016
+
+@author: Behrooz
+'''
+# this lines add the sequence classification submodule to the python path
+import sys
+sys.path.append('../sequence_analysis/src')
+
+#############################
+import sequence_train
+
+#############################
+import os
+import tensorflow as tf
+from sequence_reader import Sequence_Reader
+import sequence_train
+
+############################
+
+flags = tf.app.flags
+
+# input format
+flags.DEFINE_string("input_format", tf.float32, "input format tf.float32 or tf.float64")
+flags.DEFINE_string("data_dir", ".", "The data Directory")
+flags.DEFINE_string("train_file_range", [0, 408], "range of training files")
+flags.DEFINE_string("train_file_name_prefix", "../data/peat.genes.TSSAll.+100.-300_50_train_winsize100.seg", "Training Input files prefix")
+flags.DEFINE_string("train_file_name_postfix", "", "Training  Input files postfix")
+flags.DEFINE_string("test_file_range", [0, 170], "range of test files")
+flags.DEFINE_string("test_file_name_prefix", "../data/peat.genes.TSSAll.+100.-300_50_test_winsize100.seg", "Test Input files prefix")
+flags.DEFINE_string("test_file_name_postfix", "", "Test Input files postfix")
+
+flags.DEFINE_integer("test_iterations_in_one_train_iteration", 100, "test_iterations_in_one_train_iteration")
+
+# model parameters
+flags.DEFINE_integer("input_size", 212, "Input dimensionality [212]")
+flags.DEFINE_integer("sequence_size", 100, "Sequence_size")
+
+flags.DEFINE_integer("number_layers", 2, "Number of LSTM units")
+flags.DEFINE_integer("number_hidden", 1024, "Number of hidden units")
+
+flags.DEFINE_integer("output_size", 2, "Output dimensionality [2]")
+flags.DEFINE_integer("sequence_classifer_type", 2, "Sequence Classifier Type 1: original 2: Applies CNN")
+flags.DEFINE_integer("feature_map_size", 32, "Size of the Feature Map if 1-D Convolution is used")
+
+
+
+
+
+# hyper-parameters
+flags.DEFINE_integer("number_epochs", 20, "Number of training epochs [20]")
+flags.DEFINE_integer("batch_size", 25 , "Batch_size")
+flags.DEFINE_float("learning_rate", 0.001, "Learning rate of for Adam [0.001]")
+flags.DEFINE_float("weight_decay", 0.0005, "Weight decay [0.0005]")
+flags.DEFINE_float("beta1", 0.5, "Momentum term of Adam optimizer [0.5]")
+flags.DEFINE_float("MOVING_AVERAGE_DECAY", 0.9999, "MOVING_AVERAGE_DECAY")
+flags.DEFINE_integer("num_gpus", 4 , "MOVING_AVERAGE_DECAY")
+flags.DEFINE_float("per_process_gpu_memory_fraction", 0.4 , "per_process_gpu_memory_fraction")
+
+flags.DEFINE_boolean("dropout", False, "add drop out layer? [default:True]")
+flags.DEFINE_float("keep_prob", 0.5, "drop-out keep prob  [default:0.5]")
+flags.DEFINE_float("stddev", 0.04, "stddev for variable initialization")
+
+#
+flags.DEFINE_string("train_dir", "./log/train/", "training directory")
+
+flags.DEFINE_boolean("train_mode", True, "Should set to False in case of evaluation")
+flags.DEFINE_integer("NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN", 100, "number of training files")
+flags.DEFINE_boolean("log_device_placement", False, "log_device_placement")
+flags.DEFINE_boolean("shuffle", False, "number of training files")
+flags.DEFINE_integer("max_steps", 250000, "number of maximum iterations")
+
+flags.DEFINE_string("model_path", "./models/", "Model path [./models/]")
+flags.DEFINE_string("model_prefix", "deep3PEAT", "Model prefix [ucf101_]")
+flags.DEFINE_string("recurrent_unit", "LSTM", "Recurrent cell type [LSTM]")
+
+# env CUDA_VISIBLE_DEVICES=1 python main.py --train_mode=False --test_samples=./train_files.txt
+# env CUDA_VISIBLE_DEVICES=1 python main.py --train_mode=True --test_samples=./train_files.txt
+def main():
+    FLAGS = flags.FLAGS
+    sequence_train.train(FLAGS)
+if __name__ == "__main__":
+#   flags.FLAGS(sys.argv)
+    main()
+
